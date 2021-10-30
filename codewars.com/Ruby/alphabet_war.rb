778595b5943a10088147c4daada292408a5a5fe5
DANKOS-the-FIRST <danilkos2013@gmail.com>
2021-10-30 17:31:07 +0300

    7 kyu updated

--- codewars.com/Ruby/alphabet_war.rb
+++ codewars.com/Ruby/alphabet_war.rb
@@ -1,11 +1,14 @@
+def strength(sym)
+  ({ w: -4, p: -3, b: -2, s: -1,
+     m: 4, q: 3, d: 2, z: 1 }[sym.to_sym] || 0)
+end
+
 def alphabet_war(fight)
-  @ret = 0
-  @weight = { w: -4, p: -3, b: -2, s: -1, m: 4, q: 3, d: 2, z: 1 }
-  calculate_weight = ->(sym) { @ret += @weight[sym.to_sym] }
-  fight.delete("^[wpbsmqdz]").each_char { |x| calculate_weight.call x }
-  return "Right side wins!" if @ret > 0
-  return "Let's fight again!" if @ret == 0
-  "Left side wins!"
+  {
+    -1 => 'Left side wins!',
+    0 => "Let's fight again!",
+    1 => 'Right side wins!'
+  }[fight.chars.map { |x| strength x }.sum <=> 0]
 end
 
 puts alphabet_war "wmso"
